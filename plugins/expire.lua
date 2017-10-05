52404e67eb7dbd4e1c99cbbef4d3d5cb487ce32d
mp4saeed <32533538+mp4saeed@users.noreply.github.com>
2017-10-05 13:44:49 +0330

    saeed
    saeedi

--- plugins/expire.lua
+++ plugins/expire.lua
@@ -1,6 +1,5 @@
------my_name_is_ehsan*#ربات میخای بیا پی وی
-👇👇
------@Saee_d_28    saeed
+-----my_name_is_ehsan*#@mafia_boy
+-----@Saee_d_28     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
 local function pre_process(msg)
  msg.text = msg.content_.text
@@ -59,7 +58,6 @@ return {
   pre_process = pre_process
 }
 -- http://bombangteam.blogfa.com
------my_name_is_ehsan*#ربات میخای بیا پی وی
-👇👇
------@Saee_d_28     saeed
+-----my_name_is_ehsan*#@mafia_boy
+-----@Saee_d_28     FOR UPDATE
 -----لطفا پیام بالا رو پاک نکنید
